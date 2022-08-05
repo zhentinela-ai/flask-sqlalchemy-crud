4038dd24da354ee5450024e1f5da56034432f568
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-05 07:41:06 -0500

    Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from routes.contacts import contacts
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://analista:123456@localhost/contactsdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/contactsdb.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+SQLAlchemy(app)
 
 app.register_blueprint(contacts)
---
+++ models/contact.py
@@ -0,0 +1,14 @@
+import email
+from utils.db import db
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, fullname, email, phone):
+        self.fullname = fullname
+        self.email = email
+        self.phone = phone
