7be8bba04a7d219e3e7e8db2a9c7a1c57d71d195
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-05 08:33:31 -0500

    Actualizar datos

--- routes/contacts.py
+++ routes/contacts.py
@@ -25,9 +25,19 @@ def add_contact():
     return redirect(url_for('contacts.home'))
 
 
-@contacts.route("/update")
-def update():
-    return "update a contact"
+@contacts.route("/update/<id>", methods=["POST", "GET"])
+def update(id):
+    contact = Contact.query.get(id)
+    if request.method == "POST":
+        contact.fullname = request.form["fullname"]
+        contact.email = request.form["email"]
+        contact.phone = request.form["phone"]
+
+        db.session.commit()
+
+        return redirect(url_for("contacts.home"))
+
+    return render_template("update.html", contact=contact)
 
 
 @contacts.route("/delete/<id>")
